bd047d6b39aceca224e4f3227a6e3d379e6876ca
Hansen Gustavo <gustavo@Lagashs-MacBook-Pro.local>
2014-08-20 13:55:33 -0300

    First Commit

---
+++ JsonParser/JsonParser.m
@@ -0,0 +1,125 @@
+//
+//  JsonParser.m
+//  JsonParser
+//
+//  Created by Gustavo on 8/20/14.
+//
+
+#import "JsonParser.h"
+#import "Property.h"
+#import <objc/runtime.h>
+
+@interface JsonParser()
+
+@property (nonatomic, strong) NSMutableArray *arrayToReturn;
+@property (nonatomic, strong) NSArray *jsonArray;
+@property (nonatomic, strong) NSMutableArray *properties;
+
+@end
+
+@implementation JsonParser
+
+-(id)parseWithData : (NSData*)data forType : (Class) type{
+    
+    id parseResult;
+    self.properties = [self getPropertiesFor:type];
+    
+    NSArray *jsonResult;
+    NSDictionary *dResult = [[NSJSONSerialization JSONObjectWithData:data
+                                                             options: NSJSONReadingMutableContainers error:nil] valueForKey:@"d"];
+    
+    if(dResult.count > 1){
+        parseResult = [self parseObjectData:dResult Type:type];
+    }
+    else if(dResult.count == 1){
+        jsonResult = [dResult valueForKey:@"results"];
+        
+        if(jsonResult.count > 1) parseResult = [self parseArrayData:jsonResult Type:type];
+        
+    }
+    else{
+        jsonResult = [NSJSONSerialization JSONObjectWithData:data options: NSJSONReadingMutableContainers error:nil];
+        
+        NSMutableArray *array = [NSMutableArray array];
+        for (NSDictionary * dictionary in jsonResult) {
+            [array addObject:[self parseObjectData:dictionary Type:type]];
+        };
+        
+        parseResult = array;
+    }
+    
+    return parseResult;
+}
+
+-(id)parseObjectData : (NSDictionary*) data Type:(Class)type{
+    
+    id returnType = [[type alloc] init];
+    
+    for (Property* property in self.properties) {
+        [self setValueFor:property Data:data Return:returnType];
+    }
+    
+    return returnType;
+}
+
+-(NSMutableArray*)parseArrayData : (NSArray*) data Type : (Class)type{
+    
+    self.arrayToReturn = [NSMutableArray array];
+    
+    for (NSDictionary *dictionary in data) {
+        [self.arrayToReturn addObject:[self parseObjectData:dictionary Type:type]];
+    }
+    
+    return self.arrayToReturn;
+}
+
+-(NSMutableArray*)getPropertiesFor : (Class)type {
+    NSMutableArray* result = [NSMutableArray array];
+    
+    Class class = type;
+    
+    do {
+        unsigned int count, i;
+        objc_property_t *properties = class_copyPropertyList(class, &count);
+        
+        for (i = 0; i < count; i++) {
+            
+            Property * property = [[Property alloc]initWith:properties[i]];
+            
+            [result addObject:property];
+        }
+        
+        free(properties);
+        class = [class superclass];
+    } while ([class superclass]);
+    
+    return result;
+}
+
+-(void)setValueFor : (Property*) property Data : (NSDictionary*) data Return : (id)returnType{
+    
+    if ([property isComplexType]) {
+        
+        if([property isString]){
+            [returnType setValue:[data valueForKeyPath:property.Name] forKeyPath:property.Name];
+        }
+        else if([property isCollection]){
+            //TODO
+        }
+        else{//complex type
+            
+            Class type = NSClassFromString(property.Type);
+            
+            NSArray * array = [self getPropertiesFor:type];
+            NSDictionary *newData = [data valueForKeyPath:property.Name];
+            for (Property* property in array) {
+                
+                [self setValueFor:property Data:newData Return:returnType];
+            }
+        }
+    }
+    else{
+        [returnType setValue:[data valueForKeyPath:property.Name] forKeyPath:property.Name];
+    }
+}
+@end
---
+++ JsonParser/Property.h
@@ -0,0 +1,22 @@
+//
+//  Property.h
+//  JsonParser
+//
+//  Created by Gustavo on 8/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+@interface Property : NSObject
+@property NSString *Type;
+@property NSString *SubStringType;
+@property NSString *Name;
+
+-(id)initWith : (objc_property_t)property;
+-(bool)isString;
+-(bool)isCollection;
+-(bool)isComplexType;
+
+@end
